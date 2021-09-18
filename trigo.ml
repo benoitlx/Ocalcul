1d4b3dc47ed9317d48aa8a605eb85964105e6bcc
rambip <apero1808@gmail.com>
2021-09-18 23:46:13 +0200

    changed string to char and added reference functions

--- trigo.ml
+++ trigo.ml
@@ -1,69 +1,89 @@
+(* TODO: exp, ln *)
+type ref_function =
+    (* toutes les fonctions de reference *)
+    | Cos
+    | Sin
+;;
+
 type ft =
     | Const of float
-    | Var of string
+    | Var of char
     | Sum of (ft * ft)
     | Prod of (ft * ft)
-    | Diff of (ft * ft)
-    | Quo of (ft * ft)
-    | Cos of ft
-    | Sin of ft
+    | Neg of ft
+    | Pow of (ft * int)
+    
+    (* plus clair de faire intervenir des fonctions de reference*)
+    | Ref of (ref_function * ft)
 ;;
+
+let strf_ref f s = match f with
+    | Cos -> "cos("^s^")"
+    | Sin -> "sin("^s^")"
+
+
+let eval_ref f x = match f with
+| Cos -> cos x
+| Sin -> sin x
+;;
+
 
 let rec strf (f: ft) :string = match f with
     | Const p -> string_of_float p
-    | Var p -> p
+    | Var p -> String.make 1 p
     | Sum (u,v) -> (strf u)^"+"^(strf v)
     | Prod (u,v) -> (strf u)^"*"^(strf v)
-    | Diff (u,v) -> (strf u)^"-"^(strf v)
-    | Quo (u,v) -> (strf u)^"/"^(strf v)
-    | Cos u -> "cos("^(strf u)^")"
-    | Sin u -> "sin("^(strf u)^")"
+    | Neg u -> "-"^(strf u)
+    | Pow (u, n) -> (strf u)^"**"^(string_of_int n)
+    | Ref (f, u) -> strf_ref f (strf u)
+;;
 
 
-let rec eval (f: ft) (affect: (string * float) list) :float = match f with
+let rec eval (f: ft) (affect: (char * float) list) :float = match f with
     | Var x -> List.assoc x affect
     | Const a -> a
     | Sum (a,b) -> (eval a affect) +. (eval b affect)
     | Prod (a,b) -> (eval a affect) *. (eval b affect)
-    | Diff (a,b) -> (eval a affect) -. (eval b affect)
-    | Quo (a,b) -> (eval a affect) /. (eval b affect)
-    | Cos x -> cos (eval x affect)
-    | Sin x -> sin (eval x affect)
+    | Neg u ->  -. (eval u affect)
+    | Pow (u, n) -> (eval u affect) ** (float_of_int n);
+    (**| Quo (a,b) -> (eval a affect) /. (eval b affect)*)
+    | Ref (f, u) -> eval_ref f (eval u affect)
 ;;
 
-let rec derivate (f: ft) (x: string) :ft = match f with
+(* fonctions mutuellement recursives: derivate depend de derivate_ref et inversement*) 
+let rec derivate (f: ft) (x: char) :ft = match f with
     | Var p when p=x -> Const 1.
     | Var _ | Const _ -> Const 0.
     | Sum (f,g) -> Sum ((derivate f x), (derivate g x))
-    | Diff (f,g) -> Diff ((derivate f x), (derivate g x))
+    | Neg u -> Neg (derivate u x)
     | Prod (u,v) -> Sum (Prod ((derivate u x), v), (Prod ((derivate v x), u)))
-    | Quo (u,v) -> Quo (Diff (Prod ((derivate u x), v), (Prod ((derivate v x), u))), (Prod (v, v)))
-    | Cos u -> Prod ((Diff ((Const 0.), (derivate u x))), (Sin u))
-    | Sin u -> Prod ((derivate u x), (Cos u))
-;;
+    | Pow (u, n) -> Prod ((derivate u x), (Pow (u, (n-1))))
+    (*| Quo (u,v) -> Quo (Diff (Prod ((derivate u x), v), (Prod ((derivate v x), u))), (Prod (v, v)))*)
+    | Ref (f, u) -> derivate_ref f u x
+and 
+derivate_ref f u x = match f with
+    | Cos -> Prod ((Neg (derivate u x)), (Ref (Sin, u)))
+    | Sin -> Prod ((derivate u x), (Ref (Cos, u)))
+
 
 let rec simplify (f: ft) :ft = match f with
-    | Prod (u,v) -> 
-            if u=(Const 0.) || v=(Const 0.) then Const 0. else
-            if u=(Const 1.) then v else
-            if v=(Const 1.) then u else
-            Prod (u,v)
-    | Sum (u,v) ->
-            if u=(Const 0.) then v else
-            if v=(Const 0.) then u else
-            Sum (u,v)
-    | Diff (u,v) ->
-            if u=(Const 0.) then v else
-            if v=(Const 0.) then u else
-            Diff (u,v)
+    | Prod (u,v) when (u=Const 0. || v=Const 0.) -> Const 0.
+    | Prod (u,v) when (u=Const 1.) -> v
+    | Prod (u,v) when (v=Const 1.) -> u
+    | Prod (u,v) when (u=v)        -> Pow (u, 2)
+    | Prod (Pow (u, n), v) when (u=v) -> Pow (u, n+1)
+    | Prod (u, Pow (v, n)) when (u=v) -> Pow (u, n+1)
+    | Sum (u,v) when (u=Const 0.) -> v
+    | Sum (u,v) when (v=Const 0.) -> u
+    | Neg u when (u=Const 0.) -> Const 0.
+    | _ -> f
 ;;
 
-let f = Sum ((Cos (Var "x")), (Prod ((Var "x"), (Var "y"))));;
-let d = derivate f "x";;
-let s = simplify (Prod (Const 1., Var "x"));;
+let f = Sum ((Ref (Cos, (Var 'x'))), (Prod ((Var 'x'), (Var 'y'))));;
+let d = derivate f 'x';;
+let s = simplify (Prod (Const 1., Var 'x'));;
 let i = simplify d;;
 
-print_endline (string_of_float (eval f [("x", 2.); ("y", 3.)]));;
 print_endline (strf f);;
 print_endline (strf d);;
 print_endline (strf i);;
